00695966714a1dd2ae611634eeef49833ce3c03a
Aditya Sharma <67048929+extinctsion@users.noreply.github.com>
2024-10-19 06:31:41 +0000

    Fixed bug NN2

--- Python/Tuitorial-2/neuralnetwork2.py
+++ Python/Tuitorial-2/neuralnetwork2.py
@@ -3,23 +3,23 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from requirement import *
 
-# input = [1,2,3,2.5]
+"""
+This module demonstrates a simple neural network layer computation.
+It calculates the output using inputs, weights, and biases.
+"""
 
-# weight = [0.2,0.8,-0.5,1.0]
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# bias = 2
+inputs = [1, 2, 3, 2.5]  # Initialising inputs
 
-# output = np.dot(input, weight) + bias
-# print(output)
+# Weights initialisation
+weights = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+]
 
-inputs = [1,2,3,2.5] #initialising inputs
-
-# weights initialisation
-weights = [[0.2,0.8,-0.5,1.0],
-            [0.5, -0.91, 0.26, -0.5],
-            [-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2,3,0.5]
+biases = [2, 3, 0.5]
 
 output = np.dot(weights, inputs) + biases
 print(output)
