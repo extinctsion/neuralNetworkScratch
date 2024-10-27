72173bfdd458ec1d7c6eb7b1a4efb925cf50f2a0
Aditya Sharma <67048929+extinctsion@users.noreply.github.com>
2024-10-27 12:00:10 +0000

    Fix warning in requirement

--- Python/requirement.py
+++ Python/requirement.py
@@ -1,13 +1,16 @@
+"""
+This module checks versions of essential libraries and prints them.
+"""
+
 import sys
+import math
 import numpy as np
 import matplotlib
 import nnfs
-import math
 
 if __name__ == "__main__":
-    print("Python: ", sys.version)
-    print("Numpy: ", np.__version__)
-    print("Matplotlib: ", matplotlib.__version__)
-    print(__name__,type(__name__))
+    print("Python:", sys.version)
+    print("Numpy:", np.__version__)
+    print("Matplotlib:", matplotlib.__version__)
+    print(__name__, type(__name__))
     print("nnfs =", nnfs.__version__)
-    # print("Math: ", math.__version__)
